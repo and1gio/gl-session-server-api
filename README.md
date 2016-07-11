c4a71e8709c883d913e90133d6d7223268fb04a3
Giorgi Maisuradze <g.maisuradze88@gmail.com>
2016-07-11 15:06:43 +0400

    some fixes

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # gl-session-server-api
+
+**start development mode**
+```
+npm run development
+```
+
+**start staging mode**
+```
+npm run staging
+```
+
+**start production mode**
+```
+npm run production
+```
--- gulpfile.js
+++ gulpfile.js
@@ -4,16 +4,16 @@ var gulp = require('gulp');
 var nodemon = require('gulp-nodemon');
 
 // start development mode
-gulp.task('server.dev', function () {
+gulp.task('server.development', function () {
     return nodemon({restartable: "rs", ignore: ["*.*"], script: 'server/bin/www', env: {'NODE_ENV': 'development'}});
 });
 
 // start staging mode
-gulp.task('server.stag', function () {
+gulp.task('server.staging', function () {
     return nodemon({restartable: "rs", ignore: ["*.*"], script: 'server/bin/www', env: {'NODE_ENV': 'staging'}});
 });
 
 // start production mode
-gulp.task('server.prod', function () {
+gulp.task('server.production', function () {
     return nodemon({restartable: "rs", ignore: ["*.*"], script: 'server/bin/www', env: {'NODE_ENV': 'production'}});
 });
