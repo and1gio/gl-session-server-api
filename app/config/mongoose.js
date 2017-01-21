74da49e1f3c860aad9da3d2feda9a20699a755fe
Giorgi Maisuradze <g.maisuradze88@gmail.com>
2017-01-21 19:11:15 +0400

    fixed mongoose config

--- app/config/mongoose.js
+++ app/config/mongoose.js
@@ -16,7 +16,8 @@ exports.default = function (app) {
 exports.staging = function (app) {
     return {
         mongoose: {
-            uri: 'mongodb://192.168.4.169:27017/msda-sessions-store'
+            uri: 'mongodb://192.168.4.169:27017/msda-sessions-store',
+            modelsDir: 'models'
         }
     }
 };
@@ -27,7 +28,8 @@ exports.staging = function (app) {
 exports.production = function (app) {
     return {
         mongoose: {
-            uri: 'mongodb://192.168.1.183:27017/msda-sessions-store'
+            uri: 'mongodb://192.168.1.183:27017/msda-sessions-store',
+            modelsDir: 'models'
         }
     }
 };
